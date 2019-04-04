e36113428b464a6e44ba2d89eebb2a3832d01cbb
wuyantian <1793274552@qq.com>
2019-04-04 23:17:32 +0800

    添加刷新条

--- MaterialDesignTest/app/src/main/java/com/example/materialdesigntest/MainActivity.java
+++ MaterialDesignTest/app/src/main/java/com/example/materialdesigntest/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private NavigationView navigationView;
     private FloatingActionButton fab;
+    private SwipeRefreshLayout swipeRefreshLayout;
     private Fruit[]fruits={new Fruit("apple",R.drawable.apple_pic),new Fruit("banana",R.drawable.banana_pic)
     ,new Fruit("cherry",R.drawable.cherry_pic),new Fruit("grape",R.drawable.grape_pic)
             ,new Fruit("mango",R.drawable.mango_pic),
@@ -80,6 +82,15 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         adapter=new FruitAdapter(fruitList);
         recyclerView.setAdapter(adapter);
+        swipeRefreshLayout=findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                refreshFruits();
+
+            }
+        });
 
     }
 
@@ -115,5 +126,25 @@ public class MainActivity extends AppCompatActivity {
             int index=random.nextInt(fruits.length);
             fruitList.add(fruits[index]);
         }
+    }
+    private void refreshFruits(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+           try{
+               Thread.sleep(2000);
+           }catch (Exception e){
+               e.printStackTrace();
+           }
+           runOnUiThread(new Runnable() {
+               @Override
+               public void run() {
+                   initFruits();
+                   adapter.notifyDataSetChanged();//通知数据发生变化
+                   swipeRefreshLayout.setRefreshing(false);//刷新结束
+               }
+           });
+            }
+        }).start();
     }
 }
